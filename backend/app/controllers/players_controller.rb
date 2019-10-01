3e8367cbfce5df2d0d4876de3b0c6e093f9e3995
github username <github email address>
2019-10-01 18:51:07 -0400

    just general file clean up

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -9,4 +9,11 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
     render json: @player.to_json
   end
+
+  def create
+    @player = Player.create!(:name => params[:name], :age => params[:age], :position => params[:position], :team_id => params[:team_id])
+    render json: @player.to_json
+
+  end
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+ get '/teams/:id/players', to: 'teams#players'
   resources :players
   resources :teams
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
